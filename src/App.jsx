9a364d8e347f6018a5fefdf224c9b6b294a10173
Subhash Satyarthi <120436794+subhashprogrammer@users.noreply.github.com>
2024-11-21 12:22:59 +0530

    use redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Body from './Body'
-import Login from './Login'
-import Profile from './Profile'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Body from "./components/Body";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+import { Provider } from "react-redux";
 
 const App = () => {
-  return (<>
-  <BrowserRouter basename='/'>
-  <Routes>
-  <Route path='/' element={<Body/>}>
-    <Route path='/login' element={<Login/>}/>
-    <Route path='/profile' element={<Profile/>}/>
-  </Route>
-</Routes>
-</BrowserRouter>
-</>)}
+  return (
+    <>
+      <Provider>
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route path="/" element={<Body />}>
+              <Route path="/login" element={<Login />} />
+              <Route path="/profile" element={<Profile />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
+    </>
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/Login.jsx
@@ -0,0 +1,70 @@
+import axios from 'axios';
+import React, { useState } from 'react'
+
+const Login = () => {
+    const [eMail, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleLogin = async ()=>{
+      try {
+        const res = await axios.post("http://localhost:7777/login",{eMail:eMail,password:password,},{withCredentials:true})
+        console.log(res);
+        
+      } catch (err) {
+        console.log(err);
+        
+      }
+    }
+  return (
+    <div>
+    <div className="card bg-base-100 w-96 shadow-xl mx-auto">
+  <div className="card-body">
+    <h2 className="card-title mx-auto">LOGIN</h2>
+   <label className="input input-bordered flex items-center gap-2">
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 16 16"
+    fill="currentColor"
+    className="h-4 w-4 opacity-70">
+    <path
+      d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+    <path
+      d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
+  </svg>
+  <input 
+  type="text" 
+  className="grow" 
+  placeholder="Email"
+  value={eMail}
+  onChange={(e)=>setEmail(e.target.value)}
+   />
+</label>
+<label className="input input-bordered flex items-center gap-2">
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 16 16"
+    fill="currentColor"
+    className="h-4 w-4 opacity-70">
+    <path
+      fillRule="evenodd"
+      d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+      clipRule="evenodd" />
+  </svg>
+  <input 
+  type="password" 
+  className="grow"
+  value={password}
+  onChange={(e)=>setPassword(e.target.value)}
+
+  />
+</label>
+    <div className="card-actions justify-center">
+      <button className="btn btn-primary" onClick={handleLogin}>login</button>
+    </div>
+  </div>
+</div>
+    </div>
+  )
+}
+
+export default Login
