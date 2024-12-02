66155fa5b658f62cf48d20c6f52046703f7de36c
Subhash Satyarthi <120436794+subhashprogrammer@users.noreply.github.com>
2024-12-02 11:23:28 +0530

    change navbar color

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "https://devtinderb.onrender.com";
-// export const BASE_URL = "http://localhost:7777";
+// export const BASE_URL = "https://devtinderb.onrender.com";
+export const BASE_URL = "http://localhost:7777";
 
