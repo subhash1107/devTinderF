23dae1f145d8582258f1771c846c354452befe2d
Subhash Satyarthi <120436794+subhashprogrammer@users.noreply.github.com>
2024-12-05 21:16:41 +0530

    handlelogin

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -20,6 +20,7 @@ const Login = () => {
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const isLoading = useSelector((store)=>store.loading.isLoading)
+    const user = useSelector(store=>store.user)
     
     const handleLogin = async ()=>{
       try {
@@ -28,7 +29,8 @@ const Login = () => {
         const res = await axios.post(BASE_URL+"/login",{eMail:eMail,password:password,});
         localStorage.setItem('token1', res.data.token);
         dispatch(addUser(res.data.user))
-        navigate('/feed')
+        if(user){
+        navigate('/feed')}
         setError("")
       } catch (err) {
         setError(err?.response?.data || "Something went wrong")
