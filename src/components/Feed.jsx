8ddccb5d731fe5f334ee207cbe56d3f287cf35c8
Subhash Satyarthi <120436794+subhashprogrammer@users.noreply.github.com>
2025-07-13 16:39:57 +0530

    design

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,57 +1,62 @@
 import axios from "axios";
-import React, { useEffect, useState } from 'react'
-import {  BASE_URL } from '../utils/constants'
-import { useDispatch, useSelector } from 'react-redux'
-import { addFeed } from '../utils/feedSlice'
-import UserCard from './UserCard'
-import Loading from './Loading'
-import { setLoading } from '../utils/loadingSlice'
-import handleClearCache from '../utils/handleClearCache'
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { BASE_URL } from "../utils/constants";
+import { addFeed } from "../utils/feedSlice";
+import { setLoading } from "../utils/loadingSlice";
+import Loading from "./Loading";
+import UserCard from "./UserCard";
 
 const Feed = () => {
-    const dispatch = useDispatch();
-    const feed = useSelector((store)=>{return store.feed})
-    const isLoading = useSelector((store)=>store.loading.isLoading)
-    // console.log('Feed array as JSON:', JSON.stringify(feed, null, 2));     
+  const dispatch = useDispatch();
+  const feed = useSelector((store) => {
+    return store.feed;
+  });
+  const isLoading = useSelector((store) => store.loading.isLoading);
+  // console.log('Feed array as JSON:', JSON.stringify(feed, null, 2));
 
-    const feedData = async ()=>{
-        dispatch(setLoading(true))
-        try {
-            if(feed) return;
-            
-            const res = await axios.get(BASE_URL + "/user/feed",)
-            dispatch(addFeed(res.data.message)); 
-        } catch (err) {
-            console.log(err);
-        } finally{
-            dispatch(setLoading(false));                                             
+  const feedData = async () => {
+    dispatch(setLoading(true));
+    try {
+      if (feed) return;
 
-        }
+      const res = await axios.get(BASE_URL + "/user/feed");
+      dispatch(addFeed(res.data.message));
+    } catch (err) {
+      console.log(err);
+    } finally {
+      dispatch(setLoading(false));
     }
-     
-    useEffect(() => {    
-        const token = localStorage.getItem("token1")
-        if(token){
-          feedData();
-        }
-      }, []); // Empty array means this runs once when the component mounts
-      
-    if(isLoading) return <Loading/>
-    if(!feed) return;
-    if(feed.length===0){
-        return(<>
-                    <div className="toast toast-center toast-middle">
+  };
+
+  useEffect(() => {
+    const token = localStorage.getItem("token1");
+    if (token) {
+      feedData();
+    }
+  }, []); // Empty array means this runs once when the component mounts
+
+  if (isLoading) return <Loading />;
+  if (!feed) return;
+  if (feed.length === 0) {
+    return (
+      <>
+        <div className="toast toast-center toast-middle">
           <div className="alert alert-info">
             <span>No user found in you feed!!</span>
           </div>
         </div>
-        </>)
-    }
+      </>
+    );
+  }
 
-  return (<>
-  <div className='flex justify-center p-5 bg-base-100'>
-    {feed&&<UserCard user = {feed[0]}/>}
-    </div>
-    </>)}
+  return (
+    <>
+      <div className="flex justify-center p-5 bg-base-100">
+        {feed && <UserCard user={feed[0]} />}
+      </div>
+    </>
+  );
+};
 
-export default Feed
+export default Feed;
