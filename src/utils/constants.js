5a48b4b5d82390c4c6bdd810b4b464339ee80b4b
Subhash Satyarthi <120436794+subhashprogrammer@users.noreply.github.com>
2025-08-24 23:43:45 +0530

    changed base url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 // export const BASE_URL = "https://devtinderb.onrender.com";
 // export const BASE_URL = "http://localhost:8000";
-export const BASE_URL = "http://13.204.79.54:8000";
+export const BASE_URL = "http://3.108.67.128:8000";
 
